54205bd9d2a79cef86d6f5cfd3ae25beacea9759
unknown <chidi@chidi-THINK.(none)>
2012-03-04 11:44:02 +0000

    Initial commit

---
+++ Battleships/Grid.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Battleships
+{
+    public class Grid
+    {
+        private readonly int _height;
+        private readonly int _width;
+
+        public Grid(int height, int width)
+        {
+            _height = height;
+            _width = width;
+        }
+
+
+        public int Width()
+        {
+            return _width;
+        }
+
+        public int Height()
+        {
+            return _height;
+        }
+    }
+}
---
+++ UnitTests/GridTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Battleships;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class GridTests
+    {
+        private Grid grid;
+
+        [SetUp]
+        public void Setup()
+        {
+            grid = new Grid(10, 10);
+        }
+
+        [Test]
+        public void InitalizesATwoDimensionalArrayWithCorrectWidth()
+        {
+            Assert.AreEqual(10, grid.Width());
+        }
+        
+        [Test]
+        public void InitalizesATwoDimensionalArrayWithCorrectHeight()
+        {
+            Assert.AreEqual(10, grid.Height());
+        }
+
+        
+    }
+}

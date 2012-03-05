7692ff54a28a01f0d847af5eeac47b2527b4c651
Michael Okarimia <git@michaelokarimia.com>
2012-03-05 22:24:27 +0000

    Game state has naive initialization

--- Battleships/Gamestate.cs
+++ Battleships/Gamestate.cs
@@ -1,12 +1,32 @@
-﻿namespace Battleships
+﻿using System;
+using Battleships.Enums;
+using Battleships.Ships;
+
+namespace Battleships
 {
     public class Gamestate
     {
-        private bool _isGameOver;
+        private Board board = new Board(10, 10);
+        private bool _IsShipDeploymentPhase;
 
+        public Gamestate()
+        {
+            _IsShipDeploymentPhase = true;
+        }
         public bool IsGameOver()
         {
-            return _isGameOver;
+            return false;
+        }
+
+        public bool IsShipDeploymentPhase()
+        {
+            return _IsShipDeploymentPhase;
+        }
+
+        public void AddPlayerShip(Position position, IShip vessel)
+        {
+            board.AddShip(vessel, position);
+            _IsShipDeploymentPhase = !board.AreAllShipsPlaced();
         }
     }
 }
--- UnitTests/GameStateTests.cs
+++ UnitTests/GameStateTests.cs
@@ -1,4 +1,6 @@
 ﻿using Battleships;
+using Battleships.Enums;
+using Battleships.Ships;
 using NUnit.Framework;
 
 namespace UnitTests
@@ -18,6 +20,24 @@ namespace UnitTests
         public void GameStateInitializesCorrectly()
         {
             Assert.False(gamestate.IsGameOver());
+        }
+
+        [Test]
+        public void InitalizesInShipDeploymentPhase()
+        {
+            Assert.True(gamestate.IsShipDeploymentPhase());
+        }
+
+        [Test]
+        public void ShipDeploymentEndsWhenAllShipsPlaced()
+        {
+            gamestate.AddPlayerShip(new Position(0, 9, Orientation.Horizontal), new AircraftCarrier());
+            gamestate.AddPlayerShip(new Position(0, 0, Orientation.Horizontal), new BattleShip());
+            gamestate.AddPlayerShip(new Position(4, 1, Orientation.Horizontal), new Submarine());
+            gamestate.AddPlayerShip(new Position(4, 3, Orientation.Vertical), new Destroyer());
+            gamestate.AddPlayerShip(new Position(7, 7, Orientation.Vertical), new Minesweeper());
+
+            Assert.False(gamestate.IsShipDeploymentPhase());
         }
     }
 }
